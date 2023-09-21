78a6ed1b28ad5a6a7233298ef378f2b60052d1f9
cucuctt1 <145274381+cucuctt1@users.noreply.github.com>
2023-09-21 13:32:03 +0700

    Add files via upload

--- app.js
+++ app.js
@@ -1,12 +1,19 @@
 const express = require('express');
 const path = require('path')
 const app = express();
-
+const fs = require('fs');
 app.use(express.urlencoded());
 app.set('views',__dirname+'/views')
 app.use(express.static(path.join(__dirname,'public')))
 app.set('view engine','hbs')
 let link
+fs.readFile('./data.txt', 'utf8', (err, data) => {
+    if (err) {
+      console.error(err);
+      return;
+    }
+    link =data;
+  });
 app.get('/', function(request, response, next){
     response.render('index.hbs',{message:link})
 
@@ -15,6 +22,12 @@ app.get('/', function(request, response, next){
 app.post('/', function(request, response, next){
     let {first_name} = request.body;
     link = first_name
+    fs.writeFile('./data.txt', link, err => {
+        if (err) {
+          console.error(err);
+        }
+        // file written successfully
+      });
     response.redirect('/')
 
 });
